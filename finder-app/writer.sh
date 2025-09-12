e8ef5a9182ac671ae831807d9df70173a63dc2a1
Bigeng Wang <bw2482@columbia.edu>
2025-09-12 11:14:24 -0700

    Added writer.sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+writefile="$1"
+writestr="$2"
+
+
+if [ -z "${writefile}" ] || [ -z "${writestr}" ]; then
+	echo "ERROR: Two arguements [writefile] [writestr] must be provided."
+	exit 1
+fi
+
+
+if ls "${writefile}" > /dev/null 2>&1; then
+	echo "File exists."
+else
+	echo "File doesn't exist. Create now."
+	if ! touch "${writefile}"; then
+		echo "ERROR: faild to create ${writefile}."
+		exit 1
+	fi
+fi
+
+echo "Writing string ${writestr} to file ${writefile}"
+
+if ! echo "${writestr}" > ${writefile}; then
+	echo "ERROR: write failure."
+	exit 1
+else
+	echo "Done!"
+	exit 0
+fi
+
+
+
