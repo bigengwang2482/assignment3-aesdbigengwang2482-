8bb22523ee373aea3432eb15ba67b389bb5e0e0d
Bigeng Wang <bw2482@columbia.edu>
2025-09-20 21:52:57 -0700

    Add starting draft. Distinguish the variables and the target which are separted by space in 'Building the Linux Kernel' video.

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,46 +35,51 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper # deep clean
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig # do the configure
+	make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all # build the kernel image for booting with QEMU
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules # build any kernel modules
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs # build the device tree
 fi
 
-echo "Adding the Image in outdir"
-
-echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
-fi
-
-# TODO: Create necessary base directories
-
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
-else
-    cd busybox
-fi
-
-# TODO: Make and install busybox
-
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
-
-# TODO: Add library dependencies to rootfs
-
-# TODO: Make device nodes
-
-# TODO: Clean and build the writer utility
-
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
-
-# TODO: Chown the root directory
-
-# TODO: Create initramfs.cpio.gz
+#echo "Adding the Image in outdir"
+#
+#echo "Creating the staging directory for the root filesystem"
+#cd "$OUTDIR"
+#if [ -d "${OUTDIR}/rootfs" ]
+#then
+#	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+#    sudo rm  -rf ${OUTDIR}/rootfs
+#fi
+#
+## TODO: Create necessary base directories
+#
+#cd "$OUTDIR"
+#if [ ! -d "${OUTDIR}/busybox" ]
+#then
+#git clone git://busybox.net/busybox.git
+#    cd busybox
+#    git checkout ${BUSYBOX_VERSION}
+#    # TODO:  Configure busybox
+#else
+#    cd busybox
+#fi
+#
+## TODO: Make and install busybox
+#
+#echo "Library dependencies"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+#
+## TODO: Add library dependencies to rootfs
+#
+## TODO: Make device nodes
+#
+## TODO: Clean and build the writer utility
+#
+## TODO: Copy the finder related scripts and executables to the /home directory
+## on the target rootfs
+#
+## TODO: Chown the root directory
+#
+## TODO: Create initramfs.cpio.gz
